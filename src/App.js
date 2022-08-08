b29880f3f8d47e8bbf00a3d92901b525e8ff0c86
VladVasilegin <Vasilegin@gmail.com>
2022-08-08 11:02:43 +0300

    Studied states in react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 function App() {
   const [counter, setCounter] = useState(10);
+  const [value, setValue] = useState('Текст, который можно изменить')
 
   function Inc() {
     setCounter(counter + 1)
@@ -11,12 +12,21 @@ function App() {
     setCounter(counter - 1)
   }
 
+  function setVal() {
+    let text = 
+    setValue('другой текст')
+  }
+
+
   // console.log(counter, setCounter)
   return (
     <div>
       <h1>{counter}</h1>
       <button onClick={Inc}>Inc</button>
       <button onClick={Dec}>Dec</button>
+      <h1>{value}</h1>
+      <input type="text" name="val" value={value} onChange={event => setValue(event.target.value)}></input>
+      <button onClick={setVal}>Изменить текст</button>
     </div>
     );
 }
