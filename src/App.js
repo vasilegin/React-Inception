4b221cf1ace6a1d170f7241293d8b3b815fb1ed7
VladVasilegin <Vasilegin@gmail.com>
2022-08-08 10:32:02 +0300

    Added Inc, dec functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 function App() {
+  let counter = 5;
+
+  function inc() {
+    counter +=1
+    console.log(counter)
+  };
+
+  function dec() {
+    counter -=1
+    console.log(counter)
+  };
+
+
   return (
-    <div className="App">
-      Привет, вот прикол!
+    <div>
+      <h1>{counter}</h1>
+          <button onClick={inc}>Inc</button>
+          <button onClick={dec}>Dec</button>
     </div>
-  );
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  React.createElement('button', {
-    onClick: () => console.log("Click")
-  }, React.createElement('div', {}, "Жми"))
+  <App></App>
 );
